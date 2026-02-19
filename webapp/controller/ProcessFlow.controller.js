fe038ffdf241714e7a64de2e5ba76a1fe48df7a1
AlissCava <cavalleraalissa@gmail.com>
2026-02-19 11:02:19 +0100

    Step 9: Delta Micro Chart

--- webapp/controller/ProcessFlow.controller.js
+++ webapp/controller/ProcessFlow.controller.js
@@ -1,14 +1,18 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/base/strings/formatMessage"
+	"sap/ui/core/mvc/Controller",
+	"sap/base/strings/formatMessage"
 ], function (Controller, formatMessage) {
-    "use strict";
+	"use strict";
 
-    return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.07.controller.ProcessFlow", {
-        formatMessage: formatMessage,
+	return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.09.controller.ProcessFlow", {
+		formatMessage: formatMessage,
 
-        onNavButtonPressed: function () {
-            this.getOwnerComponent().getRouter().navTo("home");
-        }
-    });
+		onNavButtonPressed: function () {
+			this.getOwnerComponent().getRouter().navTo("home");
+		},
+
+		getValuesDelta: function (fFirstValue, fSecondValue) {
+			return fSecondValue - fFirstValue;
+		}
+	});
 });
