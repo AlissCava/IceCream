36444659e136648d1cbf6f910b1ef129db21832a
AlissCava <cavalleraalissa@gmail.com>
2026-02-19 14:16:34 +0100

    Step 11: Process Flow

--- webapp/controller/ProcessFlow.controller.js
+++ webapp/controller/ProcessFlow.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
 	"sap/base/strings/formatMessage"
-], function (Controller, formatMessage) {
+], function (Controller, MessageToast, formatMessage) {
 	"use strict";
 
-	return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.09.controller.ProcessFlow", {
+	return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.11.controller.ProcessFlow", {
 		formatMessage: formatMessage,
 
 		onNavButtonPressed: function () {
@@ -13,6 +14,15 @@ sap.ui.define([
 
 		getValuesDelta: function (fFirstValue, fSecondValue) {
 			return fSecondValue - fFirstValue;
+		},
+
+		onNodePressed: function (oEvent) {
+			var sItemTitle = oEvent.getParameters().getTitle();
+			MessageToast.show(this.getResourceBundle().getText("processFlowNodeClickedMessage", [sItemTitle]));
+		},
+
+		getResourceBundle: function () {
+			return this.getOwnerComponent().getModel("i18n").getResourceBundle();
 		}
 	});
 });
